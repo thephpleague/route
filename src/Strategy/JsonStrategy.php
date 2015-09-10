b93195bb831ae5d0e446bb384c7374a2e2faf8b1
Phil Bennett <philipobenito@gmail.com>
2015-09-10 11:57:26 +0100

    Update strategies

---
+++ src/Strategy/JsonStrategy.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace League\Route\Strategy;
+
+use ArrayObject;
+use League\Route\Http\Exception as HttpException;
+use Psr\Http\Message\ResponseInterface;
+use RuntimeException;
+
+class JsonStrategy extends AbstractStrategy implements StrategyInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function dispatch(callable $controller, array $vars)
+    {
+        try {
+            $response = call_user_func_array($controller, [
+                $this->getRequest(),
+                $vars
+            ]);
+
+            if (is_array($response) || $response instanceof ArrayObject) {
+                $body     = json_encode($response);
+                $response = $this->getResponse();
+
+                if ($response->getBody()->isWritable()) {
+                    $response->getBody()->write($body);
+                }
+            }
+
+            if ($response instanceof ResponseInterface) {
+                return $response->withAddedHeader('content-type', 'application/json');
+            }
+        } catch (HttpException $e) {
+            return $e->buildJsonResponse($this->getResponse());
+        }
+    }
+}
