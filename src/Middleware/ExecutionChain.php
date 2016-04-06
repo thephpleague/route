c40f0b5c4fd57480a83aac3ee708954eec17997b
Phil Bennett <philipobenito@gmail.com>
2016-04-06 13:41:44 +0100

    Cleanup tasks

--- src/Middleware/ExecutionChain.php
+++ src/Middleware/ExecutionChain.php
@@ -2,8 +2,8 @@
 
 namespace League\Route\Middleware;
 
-use \Psr\Http\Message\ResponseInterface;
-use \Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
 
 class ExecutionChain implements StackAwareInterface
 {
