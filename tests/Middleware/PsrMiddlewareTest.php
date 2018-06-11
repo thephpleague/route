e24fc69942111db144a0ceaf155f59d36a49563d
Andrew Willis <andrew@willishq.co.uk>
2018-06-12 00:10:59 +0100

    asserts that multiple types of middleware are called correctly

--- tests/Middleware/PsrMiddlewareTest.php
+++ tests/Middleware/PsrMiddlewareTest.php
@@ -3,6 +3,7 @@
 namespace Middleware;
 
 use League\Route\Middleware\ExecutionChain;
+use League\Route\Test\Asset\Controller;
 use League\Route\Test\Asset\PSR15Middleware;
 
 class PsrMiddlewareTest extends \PHPUnit_Framework_TestCase
@@ -45,5 +46,33 @@ class PsrMiddlewareTest extends \PHPUnit_Framework_TestCase
 
         $this->assertSame($response, $executedResponse);
         $this->assertEquals(2, $middleware->getCalls());
+    }
+
+    public function testItCanProcessPSR15andNonPSR15Middlewares()
+    {
+        $chain = new ExecutionChain();
+        $psr15Middleware = new PSR15Middleware();
+        $middleware = new Controller();
+        $request  = $this->getMock('Psr\Http\Message\ServerRequestInterface');
+        $response = $this->getMock('Psr\Http\Message\ResponseInterface');
+
+        $response->expects($this->at(0))->method('withHeader')->with($this->equalTo('invoke'), $this->equalTo('true'))->will($this->returnSelf());
+        $response->expects($this->at(1))->method('withHeader')->with($this->equalTo('action'), $this->equalTo('true'))->will($this->returnSelf());
+
+        $chain->middleware($psr15Middleware);
+        $chain->middleware([$middleware, 'action']);
+        $chain->middleware($middleware);
+        $calls = 0;
+        $chain->middleware(function ($request, $response, $next) use (&$calls) {
+            $calls++;
+            return $next($request, $response);
+        });
+
+        $executedResponse = $chain->execute($request, $response);
+
+        $this->assertSame($response, $executedResponse);
+        $this->assertEquals(1, $psr15Middleware->getCalls());
+        $this->assertEquals(1, $calls);
+
     }
 }
